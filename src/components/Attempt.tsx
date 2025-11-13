6e6da7dfef9b781da91dee03371c659446099789
Ben Montgomery <ben@modernthings.net>
2025-11-13 11:52:07 -0700

    Remove another border-1

--- src/components/Attempt.tsx
+++ src/components/Attempt.tsx
@@ -26,7 +26,7 @@ function Attempt({
   };
 
   return (
-    <div className="relative mx-4 my-2 min-h-13 rounded-md border-1 border-gray-500 bg-white p-2 shadow-sm last:border-dashed last:bg-blue-100">
+    <div className="relative mx-4 my-2 min-h-13 rounded-md border border-gray-500 bg-white p-2 shadow-sm last:border-dashed last:bg-blue-100">
       <button
         onClick={() => deleteAttempt(index)}
         className="absolute top-0 right-1"
